55da5ba8045d0ec9a39442f099692665200b4bb5
codux31 <seneciomoreau@gmail.com>
2022-02-21 11:07:59 +0100

    First commit
    First commit
    
    Co-Authored-By: Cornillon Michael <ei.dwaps@gmail.com>

---
+++ README.md
@@ -0,0 +1,39 @@
+## Table of Contents
+1. [General Info](#general-info)
+2. [Technologies](#technologies)
+3. [Installation](#installation)
+4. [Collaboration](#collaboration)
+5. [FAQs](#faqs)
+### General Info
+***
+Cette application vous permet de libérer votre esprit en notant toute vos tâches à faire, les maruer comme faite et enfin les supprimer. L'application se déploie très simplement, avec un outil comme _LiveServ_ ou _devServ_ (Disponibles tous deux sur Visual Studio) mais vous pouvez également upload le projet dans un répertoire web. Développé entièrement par _Senecio Moreau_ pour _DWAPS_ dans le cadre du Bachelor _CSI_.
+### Screenshot
+![Image text](/todolist-screenshot.png)
+## Technologies
+***
+Liste des technologies utilisées pour développer le projet:
+* [HTML5](https://fr.wikipedia.org/wiki/HTML5): Version 5.2 
+* [CSS3](https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade): Version 3
+* [Bootstrap](https://getbootstrap.com/): Version 4.6
+* [JavaScript](https://developer.mozilla.org/fr/docs/Web/JavaScript): Version 11
+* [Visual Studio Code](https://code.visualstudio.com/): Version 1.64.2
+* [devServer](https://marketplace.visualstudio.com/items?itemName=ihuke.devServer): Version 0.0.4
+## Installation
+***
+Veuillez suivre ces instructions pour lancer l'application : 
+```
+1) Ouvrez le dossier du projet dans VSC.
+2) Lancez votre serveur local (devServer, liveServer ou autre).
+3) Rendez-vous sur la page index.html de votre host local depuis un navigateur.
+4) Vous avez normalement l'application ouverte, notez qu'il y a un espace de stockage local pour sauvegarder les tâches.
+```
+## Collaboration
+***
+Poussez en ma compagnie le projet encore plus loin...
+> Contactez-moi pour une éventuelle collaboration par mail à l'adresse 'senecio@ulpra.com'.
+> Mes projets sont disponibles sur mon site 'https://www.ulpra.com/projets/'.
+## FAQs
+***
+Liste des questions fréquentes :
+1. **Puis-je fermer mon navigateur web sans que mes tâches soient perdues ?**
+Vous pouvez effectivement fermer votre navigateur web tout en conservant les données inscrites dans la liste des tâches à la seule condition de _ne pas effacer votre cache et vos cookies_ du navigateur. 
---
+++ js/app.js
@@ -0,0 +1,108 @@
+// Sélectionner le formulaire des entrées
+const taskForm = document.querySelector('.taskForm');
+// Séléctionner les entrées du formulaire
+const inputForm = document.querySelector('.inputForm');
+// Séléctionner la liste des tâches
+const tasksList = document.querySelector('#taskList');
+
+// Tableau qui va stocker les tâches en localStorage
+let tasks = [];
+
+// Ajouter d'un eventListener sur le formulaire des entrées
+taskForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  addTask(inputForm.value);
+});
+
+// Création d'une tâche sous forme de liste
+function addTask(task) {
+  var inputText = document.getElementById("inputAddingText").value;
+  if (inputText === '') {
+    alert("Vous devez écrire une tâche pour l'ajouter à vote liste !");
+    console.log('Une tâche vide ne peut pas être ajoutée.')
+  } else {
+    const task = {
+      id: Date.now(),
+      name: inputText,
+      completed: false
+    };
+    // Ajout de la tâche au tableau des tâches
+    tasks.push(task);
+    // Sauvegarder le tableau des tâches dans le localStorage
+    addToLocalStorage(tasks);
+  }
+  inputForm.value = '';
+}
+
+function renderTasks(tasks) {
+  // Nettoyage de la liste des tâches (#taskList)
+  tasksList.innerHTML = '';
+  // Parcourir toutes les tâches du tableau des tâches
+  tasks.forEach(function (task) {
+    //Vérifier si la tâche est marquée comme faite
+    const checked = task.completed ? 'checked' : null;
+    // Créer une tâche avec la balise html liste
+    const li = document.createElement('li');
+    li.className = "list-group-item task d-flex flex-row align-items-center justify-content-center w-50";
+    // Ajouter un id
+    li.setAttribute('data-key', task.id);
+    // Si la tâche est marquée comme faite : Ajouter la classe "checked"
+    if (task.completed === true) {
+      li.classList.add('checked');
+      console.log('Tâche marquée comme faite.');
+    }
+    li.innerHTML = `<input type="checkbox" title="Marquer la tâche comme faite" class="checkbox form-check-input position-absolute start-0 mt-0" ${checked}>
+    <p class="mb-0 item">${task.name}</p> 
+    <button title="Supprimer définitivement la tâche" class="deleteTask btn btn-outline-danger position-absolute top-0 end-0 h-100">×</button>`;
+    // Ajout de la tâche à la liste
+    tasksList.append(li);
+  });
+}
+
+// Fonction permettant de sauvegarder le tableau des tâches dans le localStorage de l'utilisateur
+function addToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks(tasks);
+}
+
+// Fonction pour récupérer tout ce qu'il y a dans le localStorage
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('tasks');
+  if (reference) {
+    tasks = JSON.parse(reference);
+    renderTasks(tasks);
+  }
+}
+
+// Vérification tâche faite ou pas
+function toggle(id) {
+  tasks.forEach(function (task) {
+    if (task.id == id) {
+      task.completed = !task.completed;
+    }
+  });
+  addToLocalStorage(tasks);
+}
+
+// Fonction de suppression d'une tâche
+function deleteTask(id) {
+  tasks = tasks.filter(function (task) {
+    return task.id != id;
+  });
+  addToLocalStorage(tasks);
+}
+
+// Récupérer les tâches stockées dans le localStorage
+getFromLocalStorage();
+
+// Vérification des évennements "tâche faite" & "supprimer tâche"
+tasksList.addEventListener('click', function (event) {
+  // Vérifier si la tâche est faite
+  if (event.target.type === 'checkbox') {
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+  // Vérifier si la tâche est supprimé
+  if (event.target.classList.contains('deleteTask')) {
+    deleteTask(event.target.parentElement.getAttribute('data-key'));
+  }
+});
